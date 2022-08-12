0148cbff9fb32876c69d41b65882edf2ed494fb9
bbketen <b.batuhanketen@gmail.com>
2022-08-12 18:40:07 +0300

    configten token silindi

--- config.js
+++ config.js
@@ -2,6 +2,5 @@ module.exports = {
     channel: "994908802472222741",
     messageTemplate: "@everyone Merhaba, Yeni video yayında!! **{title}**\n Beğenmeyi ve Abone olmayı Unutmayınız Efenim!!\n {url}",
     channel_id: "UCwHxquvWTmsOJWUFY9hzbnQ",
-    watchInterval: 30000,
-    token: "MTAwNzY0OTI4NTE4NzMyMTk1Nw.Gdqi9A.uMd9XndbnOZKOXvAvCUwIeux391wRkNxMIm8lA"
+    watchInterval: 30000
 }
